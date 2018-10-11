832dc243840a72a0758a6db4c0b76ec1ea0502ba
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-10-11 15:16:23 -0400

    Finish presentation components lab

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,22 @@
-// Code SimpleComponentHere Here
+import React from 'react';
+
+class SimpleComponentHere extends React.Component {
+    constructor(){
+      super();
+
+      this.state = {
+        mood: "happy"
+      }
+    }
+
+    handleClick = () => {
+      const currentMood = this.state.mood === "happy" ? "sad" : "happy"
+      this.setState({ mood: currentMood });
+    }
+
+    render() {
+      return <div onClick={this.handleClick}>{this.state.mood}</div>;
+    }
+}
+
+export default SimpleComponentHere;
